66a6b86ccddb60d36ea690bf3de79308ea2213ee
ythosa <vasus714@yandex.ru>
2022-08-03 19:48:32 +0300

    feat: makefile and scripts

---
+++ internal/models/issue_test.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestJiraIssueID_GetURL(t *testing.T) {
+	t.Parallel()
+
+	jiraURL := "https://jira.ru"
+	jiraIssueID := "353"
+
+	assert.Equal(t, JiraIssueID(jiraIssueID).GetURL(jiraURL), "https://jira.ru/353")
+}
